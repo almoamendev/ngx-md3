2324d7860af4b9ca29f92dfccdbf0eaad1b17f27
oma1083 <ave9008@gmail.com>
2025-11-24 08:17:06 +0300

    update

--- projects/ngx-md3/src/lib/components/loading-and-progress/loading-indicator/loading-indicator.ts
+++ projects/ngx-md3/src/lib/components/loading-and-progress/loading-indicator/loading-indicator.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'md3-loading-indicator',
-  imports: [],
-  templateUrl: './loading-indicator.html',
-  styleUrl: './loading-indicator.scss',
+    selector: 'md3-loading-indicator',
+    imports: [],
+    templateUrl: './loading-indicator.html',
+    styleUrl: './loading-indicator.scss',
 })
 export class LoadingIndicator {
-
+    @Input() contained: boolean = false;
 }
