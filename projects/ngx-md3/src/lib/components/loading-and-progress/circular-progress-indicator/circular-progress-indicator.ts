cd601e4c6c1cb3d57a14352d5a79147eaff5a644
Hussain Almoamen <ave9008@gmail.com>
2025-11-29 07:21:09 +0300

    update

--- projects/ngx-md3/src/lib/components/loading-and-progress/circular-progress-indicator/circular-progress-indicator.ts
+++ projects/ngx-md3/src/lib/components/loading-and-progress/circular-progress-indicator/circular-progress-indicator.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'md3-circular-progress-indicator',
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
     styleUrl: './circular-progress-indicator.scss',
 })
 export class CircularProgressIndicator {
+    @Input() indeterminate: boolean = false;
+    @Input() thickness: 4 | 8 = 4;
+    @Input() progress: number = 0;
 
+    constructor() {
+        if (this.progress < 0) {
+            this.progress = 0;
+        }
+
+        if (this.progress > 100) {
+            this.progress = 100;
+        }
+    }
 }
