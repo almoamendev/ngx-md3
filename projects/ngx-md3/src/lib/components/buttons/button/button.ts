f1308aa1c8d829d23c6bcb6e74918040936fd66c
oma1083 <ave9008@gmail.com>
2025-12-01 15:35:10 +0300

    update

---
+++ projects/ngx-md3/src/lib/components/buttons/button/button.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'button[md3-button], a[md3-button]',
+    imports: [],
+    templateUrl: './button.html',
+    styleUrl: './button.scss',
+})
+export class Button {
+}
--- projects/ngx-md3/src/lib/components/index.ts
+++ projects/ngx-md3/src/lib/components/index.ts
@@ -1,6 +1,9 @@
 // common components and directives
 export * from './common/material-icon/material-icon';
 
+// buttons
+export * from './buttons/button/button';
+
 // loading and progress
 export * from './loading-and-progress/loading-indicator/loading-indicator';
 export * from './loading-and-progress/linear-progress-indicator/linear-progress-indicator';
