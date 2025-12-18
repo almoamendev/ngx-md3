d163ba042543916a202fb3250039533762cecc00
oma1083 <ave9008@gmail.com>
2025-12-18 08:29:35 +0300

    add error state styling

--- projects/ngx-md3/src/lib/components/text-field/text-field.ts
+++ projects/ngx-md3/src/lib/components/text-field/text-field.ts
@@ -2,6 +2,7 @@ import { AfterContentInit, Component, ContentChild, ContentChildren, DestroyRef,
 import { TextInput } from './text-input';
 import { IconElement } from '../common/icon-element';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AbstractControl, FormControlName } from '@angular/forms';
 
 @Component({
     standalone: false,
@@ -12,23 +13,39 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 export class TextField implements AfterContentInit {
     @Input() label?: string;
     @Input() type: 'filled' | 'outlined' = 'filled';
+    @Input() control?: AbstractControl;
 
     @ViewChild('inputContainer', { static: true }) inputContainer!: ElementRef<HTMLDivElement>;
     @ContentChild(TextInput) input?: TextInput;
     @ContentChildren(IconElement, { descendants: true }) iconElements?: QueryList<IconElement>;
+    @ContentChild(FormControlName) controlName?: FormControlName;
 
     constructor(private destroyRef: DestroyRef) { }
 
     ngAfterContentInit(): void {
-        if (this.input) {
-            this.input.nativeElement.setAttribute('placeholder', '');
-        }
+        this.input?.nativeElement.setAttribute('placeholder', '');
 
         this.applyIconClasses();
 
         this.iconElements?.changes.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
             this.applyIconClasses();
         });
+
+        this.formControl?.statusChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
+            if (this.formControl?.invalid && (this.formControl?.touched || this.formControl?.dirty)) {
+                this.input?.nativeElement.setAttribute('aria-invalid', 'true');
+            } else {
+                this.input?.nativeElement.setAttribute('aria-invalid', 'false');
+            }
+        });
+    }
+
+    public get formControl(): AbstractControl | undefined {
+        if (this.control) {
+            return this.control;
+        }
+
+        return this.controlName?.control;
     }
 
     private applyIconClasses(): void {
