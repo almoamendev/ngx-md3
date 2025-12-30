7d2f158b111108fc44aad1056e38ed0104751e79
Hussain Almomen <ave9008@gmail.com>
2025-12-30 15:39:20 +0300

    update

--- projects/ngx-md3/src/lib/components/text-field/text-field-module.ts
+++ projects/ngx-md3/src/lib/components/text-field/text-field-module.ts
@@ -4,10 +4,11 @@ import { TextInput } from './text-input';
 import { IconElement } from '../common/icon-element';
 import { SupportingText } from './supporting-text';
 import { IconButton } from '../buttons/icon-button/icon-button';
+import { NgClass } from '@angular/common';
 
 @NgModule({
     declarations: [TextField, TextInput, SupportingText],
-    imports: [IconElement, IconButton],
+    imports: [NgClass, IconElement, IconButton],
     exports: [TextField, TextInput, SupportingText, IconElement, IconButton],
 })
 export class TextFieldModule { }
