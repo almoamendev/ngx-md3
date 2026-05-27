094e9c379e6868f46cf37937388c007b2e355c79
Hussain Almoamen <ave9008@gmail.com>
2026-05-28 00:52:04 +0300

    update

--- projects/docs/src/app/components/buttons/split-buttons/split-buttons.component.ts
+++ projects/docs/src/app/components/buttons/split-buttons/split-buttons.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { IconElement, MaterialIcon, SplitButton } from '@vip9008/ngx-md3';
 
 @Component({
-  selector: 'app-split-buttons.component',
-  imports: [],
-  templateUrl: './split-buttons.component.html',
-  styleUrl: './split-buttons.component.scss',
+    selector: 'app-split-buttons.component',
+    imports: [
+        SplitButton,
+        MaterialIcon,
+        IconElement,
+    ],
+    templateUrl: './split-buttons.component.html',
+    styleUrl: './split-buttons.component.scss',
 })
 export class SplitButtonsComponent {
+    public logAction(name: string): void {
+        console.log('Split button action:: ', name);
+    }
 
+    public logMenu(name: string): void {
+        console.log('Split button menu:: ', name);
+    }
 }
--- projects/ngx-md3/src/lib/components/buttons/split-button/split-button.ts
+++ projects/ngx-md3/src/lib/components/buttons/split-button/split-button.ts
@@ -1,13 +1,20 @@
-import { Component, effect, ElementRef, Input, signal } from '@angular/core';
+import { booleanAttribute, Component, effect, ElementRef, EventEmitter, Input, Output, signal } from '@angular/core';
 import { ButtonSize } from '../../../types/button-size.type';
 import { SplitButtonType } from '../../../types/split-button-type.type';
+import { StateComponent } from '../../common/state-component';
+import { MaterialIcon } from '../../common/material-icon/material-icon';
 
 @Component({
     selector: 'md3-split-button',
     imports: [
+        StateComponent,
+        MaterialIcon,
     ],
     templateUrl: './split-button.html',
     styleUrl: './split-button.scss',
+    host: {
+        'role': 'group',
+    },
 })
 export class SplitButton {
     @Input('button-size') set size(value: ButtonSize) {
@@ -31,6 +38,16 @@ export class SplitButton {
         });
     };
 
+    @Input({ transform: booleanAttribute }) disabled: boolean = false;
+    @Input({ alias: 'action-disabled', transform: booleanAttribute }) actionDisabled: boolean = false;
+    @Input({ alias: 'menu-disabled', transform: booleanAttribute }) menuDisabled: boolean = false;
+    @Input('action-type') actionType: 'button' | 'submit' | 'reset' = 'button';
+    @Input('menu-label') menuLabel: string = 'Show more options';
+    @Input('menu-expanded') menuExpanded: boolean | null = null;
+
+    @Output() actionClick = new EventEmitter<MouseEvent>();
+    @Output() menuClick = new EventEmitter<MouseEvent>();
+
     private buttonSize = signal<ButtonSize>('small');
     private buttonType = signal<SplitButtonType>('filled');
 
@@ -46,5 +63,33 @@ export class SplitButton {
 
     public get element(): HTMLElement {
         return this.el.nativeElement as HTMLElement;
+    }
+
+    protected get isActionDisabled(): boolean {
+        return this.disabled || this.actionDisabled;
+    }
+
+    protected get isMenuDisabled(): boolean {
+        return this.disabled || this.menuDisabled;
+    }
+
+    protected onActionClick(event: MouseEvent): void {
+        if (this.isActionDisabled) {
+            event.preventDefault();
+            return;
+        }
+
+        this.actionClick.emit(event);
+    }
+
+    protected onMenuClick(event: MouseEvent): void {
+        event.stopPropagation();
+
+        if (this.isMenuDisabled) {
+            event.preventDefault();
+            return;
+        }
+
+        this.menuClick.emit(event);
     }
 }
