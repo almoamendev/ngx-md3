7c1796dff68d57c4b36c40a52ec2110d6bc74fa4
vip9008 <ave9008@gmail.com>
2026-06-01 01:46:59 +0300

    update

--- projects/ngx-md3/src/lib/components/navigations/navigation-rail/navigation-rail.ts
+++ projects/ngx-md3/src/lib/components/navigations/navigation-rail/navigation-rail.ts
@@ -5,6 +5,7 @@ import { FloatingActionButton } from '../../buttons/floating-action-button/float
 
 type ExpandedLayout = 'standard' | 'modal';
 type CollapsedLayout = 'compact' | 'narrow' | 'hidden';
+type ContainerStyle = 'none' | 'elevated' | 'divider';
 
 @Component({
     standalone: false,
@@ -23,6 +24,15 @@ export class NavigationRail {
     public hideMenuButton = input<boolean, unknown>(false, {
         alias: 'hide-menu-button',
         transform: booleanAttribute
+    });
+
+    public fullWidthIndicator = input<boolean, unknown>(false, {
+        alias: 'full-width-indicator',
+        transform: booleanAttribute
+    });
+
+    public containerStyle = input<ContainerStyle>('none', {
+        alias: 'container-style',
     });
 
     public collapsedMode = input<CollapsedLayout>('compact', {
@@ -57,6 +67,21 @@ export class NavigationRail {
     public hasFab = computed(() => !!this.floatingActionButton());
 
     constructor() {
+        effect((onCleanup) => {
+            const containerStyle = this.containerStyle();
+            if (containerStyle == 'none') {
+                return;
+            }
+
+            const containerClass = 'md3-style-' + containerStyle;
+
+            this.element.classList.add(containerClass);
+
+            onCleanup(() => {
+                this.element.classList.remove(containerClass);
+            });
+        });
+
         effect((onCleanup) => {
             const collapsed = this.resolvedCollapsedLayout();
             const collapsedClass = 'md3-collapsed-' + collapsed;
