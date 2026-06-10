bc0bb5566643ada799948a1365c3bac631da6c4a
oma1083 <ave9008@gmail.com>
2026-06-10 15:59:32 +0300

    update

--- projects/docs/src/app/layout/layout.component.ts
+++ projects/docs/src/app/layout/layout.component.ts
@@ -2,6 +2,7 @@ import { Component, computed } from '@angular/core';
 import { RouterLink, RouterOutlet } from "@angular/router";
 import { AppBarModule, IconElement, InputElement, MaterialIcon, NavigationBarModule, NavigationRailModule, ScaffoldModule, Switch } from '@vip9008/ngx-md3';
 import { AppLayout } from './app-layout';
+import { FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-layout',
@@ -20,15 +21,21 @@ import { AppLayout } from './app-layout';
     templateUrl: './layout.component.html',
     styleUrl: './layout.component.scss',
     host: {
-        '[class.md3-scheme-dark]': 'darkMode()',
-        '[class.md3-scheme-light]': '!darkMode()',
+        '[class.md-scheme-dark]': 'darkMode()',
+        '[class.md-scheme-light]': '!darkMode()',
     },
 })
 export class LayoutComponent {
     public darkMode = computed<boolean>(() => this.layoutService.darkMode());
+    public darkModeControl: FormControl = new FormControl<boolean>(false);
 
     constructor(
         private layoutService: AppLayout
     ) {
+        this.darkModeControl.setValue(this.darkMode());
+
+        this.darkModeControl.registerOnChange(() => {
+            this.layoutService.darkMode.set(this.darkModeControl.value);
+        });
     }
 }
