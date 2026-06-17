4ef671d5550dbc20a87e1b16c75722a0ccaf4a2a
vip9008 <ave9008@gmail.com>
2026-06-18 01:20:36 +0300

    update

--- projects/docs/src/app/components/playground/playground.ts
+++ projects/docs/src/app/components/playground/playground.ts
@@ -1,14 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { Card } from '@vip9008/ngx-md3';
+import { NgClass } from '@angular/common';
+import { Component, input, signal } from '@angular/core';
+import { Button, Card, IconButton, IconElement, MaterialIcon } from '@vip9008/ngx-md3';
 
 @Component({
     selector: 'app-playground',
     imports: [
+        Button,
+        IconButton,
+        MaterialIcon,
+        IconElement,
         Card,
+        NgClass,
     ],
     templateUrl: './playground.html',
     styleUrl: './playground.scss',
 })
 export class Playground {
-    public title = signal<string>('Playground');
+    public title = input<string>('Playground');
+    public darkMode = signal<boolean>(true);
+    public rtl = signal<boolean>(false);
+
+    public toggleDarkMode() {
+        this.darkMode.update((mode) => !mode);
+    }
+
+    public toggleRtl() {
+        this.rtl.update((mode) => !mode);
+    }
 }
