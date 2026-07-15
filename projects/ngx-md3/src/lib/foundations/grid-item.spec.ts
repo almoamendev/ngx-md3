0b2b2f35517b9823665eb4f1f10ccecfb0e5677e
vip9008 <ave9008@gmail.com>
2026-07-15 13:58:05 +0300

    update

---
+++ projects/ngx-md3/src/lib/foundations/grid-item.spec.ts
@@ -0,0 +1,8 @@
+import { GridItem } from './grid-item';
+
+describe('GridItem', () => {
+  it('should create an instance', () => {
+    const directive = new GridItem();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/ngx-md3/src/lib/foundations/grid-item.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+    selector: 'md3-grid-item, [md3-grid-item]'
+})
+export class GridItem {
+    constructor() { }
+}
